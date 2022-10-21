4a8c9105964737b28df3175a4155733331107297
Abdellah Ramdan <abdodo59@gmail.com>
2022-10-21 16:01:16 +0200

    Front page d'accuei

--- deepGeneration/app/views.py
+++ deepGeneration/app/views.py
@@ -8,4 +8,15 @@ def home_page(request):
 def image_page(request):
     title = "Image Generator"
     context = {"title" : title}
-    return render(request, 'app/imageG.html', context = context)
+    return render(request, 'app/image.html', context = context)
+
+def blog_page(request):
+    title = "Blog Generator"
+    context = {"title" : title}
+    return render(request, 'app/blog.html', context = context)
+
+def code_page(request):
+    title = "Code Generator"
+    context = {"title" : title}
+    return render(request, 'app/code.html', context = context)
+
