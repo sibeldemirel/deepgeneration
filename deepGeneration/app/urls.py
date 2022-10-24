bef5bb92ebe2ddfb5092356c107e515ea358b63f
Elias Mezine <elias.mezine@laposte.net>
2022-10-24 14:58:06 +0200

    ajout des lien dans toutes les pages

--- deepGeneration/app/urls.py
+++ deepGeneration/app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_page),
+    path('home/', views.home_page, name = 'url_home'),
     path('image/', views.image_page, name = 'url_image'),
     path('blog/', views.blog_page, name = 'url_blog'),
     path('code/', views.code_page, name = 'url_code'),
