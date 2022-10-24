cde84d8704a00b326cc8395f2b199e00ee59b7d7
Abdellah Ramdan <abdodo59@gmail.com>
2022-10-24 12:18:07 +0200

    Nav bar laterale

--- deepGeneration/deepGeneration/settings.py
+++ deepGeneration/deepGeneration/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
-    'crispy_forms',
+    # 'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
