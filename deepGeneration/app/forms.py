e4684950309af0c03705a9774316a3310698e9b0
Elias Mezine <elias.mezine@laposte.net>
2022-11-02 09:31:37 +0100

    copie abdellah form

--- deepGeneration/app/forms.py
+++ deepGeneration/app/forms.py
@@ -9,4 +9,12 @@ class ApiForm(forms.ModelForm):
     
 class UserFormCustom(UserCreationForm):
     class Meta(UserCreationForm.Meta):
+        model = User
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(UserFormCustom, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class':'form-control', 'placeholder':'Nom..'})
+        self.fields['email'].widget.attrs.update({'class':'form-control','placeholder':'Email..'})
+        self.fields['password1'].widget.attrs.update({'class':'form-control','placeholder':'Mot de passe..'})
+        self.fields['password2'].widget.attrs.update({'class':'form-control','placeholder':'Confirmer mot de passe..'})
